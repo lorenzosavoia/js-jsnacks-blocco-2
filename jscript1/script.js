a8f16be893fdc799ecd5f28a55d0d87defb96f00
lorenzosavoia <lorenzo.savoia97@gmail.com>
2021-12-07 20:14:46 +0100

    end first es

--- jscript1/script.js
+++ jscript1/script.js
@@ -1,2 +1,23 @@
 // jsnack 1
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let names = ["luca ", "mario ", "lorenzo ", "marcolino ", "pazzo "];
+
+let surname = ["Savoia", "cesare", "pilato", "sgravato", "marenghi"];
+
+let nameSurname = [];
+
+
+while (nameSurname.length < 5) {
+    let randomName = getRandomInteger(0,4);
+    let randomSurname = getRandomInteger(0,4);
+    if (!nameSurname.includes(names[randomName] + surname[randomSurname])) {
+        nameSurname.push(names[randomName] + surname[randomSurname])
+        
+    }    
+}
+console.log(nameSurname);
