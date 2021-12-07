faaaa84304496af9510ed31115e23e94312bb0a3
lorenzosavoia <lorenzo.savoia97@gmail.com>
2021-12-07 20:28:31 +0100

    end

--- jscript2/script.js
+++ jscript2/script.js
@@ -4,3 +4,19 @@
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+const array = [];
+let numbers = 0;
+
+for (let i = 0; i < 10; i++) {
+    let number = getRandomInteger(1,50);
+    array.push(number);
+}
+
+for (let x = 0; x < array.length; x+=2) {
+   numbers += array[x];
+//    console.log(array[x]) 
+}
+
+console.log(numbers)
+console.log(array);
